8e9452d89ea638cbda95659f50e0edb2a1b3b67d
Peter Toth <tothpeter08@gmail.com>
2023-05-15 22:35:15 +0200

    Displaying the threads and their posts on the homepage

---
+++ forum/src/components/PageHome.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>Home page</h1>
+    <div v-for="thread in threads" :key="thread.id">
+      <h2>{{ thread.title }}</h2>
+      <div v-for="postId in thread.posts" :key="postId">
+        <p>{{ postById(postId).text }}</p>
+        <p>By {{ userById(postById(postId).userId).name }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import sourceData from '@/data.json'
+
+export default {
+  data () {
+    return {
+      threads: sourceData.threads,
+      posts: sourceData.posts,
+      users: sourceData.users
+    }
+  },
+  methods: {
+    postById(id) {
+      return this.posts.find(post => post.id === id)
+    },
+    userById(id) {
+      return this.users.find(user => user.id === id)
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
