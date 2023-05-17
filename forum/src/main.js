2a378a95a0cb0200495108936e3f9e5ec9586c0f
Peter Toth <tothpeter08@gmail.com>
2023-05-17 11:28:52 +0200

    Refactor: Move routing into its own file

--- forum/src/main.js
+++ forum/src/main.js
@@ -1,29 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { createRouter, createWebHistory } from 'vue-router'
-
-// Routing
-import PageHome from '@/components/PageHome'
-import PageThreadShow from '@/components/PageThreadShow'
-
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: PageHome,
-  },
-  {
-    path: '/thread/:id',
-    name: 'ThreadShow',
-    component: PageThreadShow,
-    props: true
-  },
-]
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes,
-})
+import router from '@/router'
 
 const app = createApp(App)
 
