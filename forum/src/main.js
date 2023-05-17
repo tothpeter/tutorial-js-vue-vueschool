0d048c830c880a54674cfe0a1be8512b52698d07
Peter Toth <tothpeter08@gmail.com>
2023-05-17 11:02:43 +0200

    Refactor the router a bit

--- forum/src/main.js
+++ forum/src/main.js
@@ -1,16 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import * as VueRouter from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 // Routing
 import PageHome from '@/components/PageHome'
 
 const routes = [
-  { path: '/', component: PageHome },
+  {
+    path: '/',
+    name: 'Home',
+    component: PageHome,
+  },
 ]
 
-const router = VueRouter.createRouter({
-  history: VueRouter.createWebHistory(),
+const router = createRouter({
+  history: createWebHistory(),
   routes,
 })
 
