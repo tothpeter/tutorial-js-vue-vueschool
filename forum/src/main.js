9ec450ab8b8be8c0a6f2a5d5c16f03f7a255a68a
Peter Toth <tothpeter08@gmail.com>
2023-05-17 10:42:13 +0200

    Add routing

--- forum/src/main.js
+++ forum/src/main.js
@@ -1,4 +1,21 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import * as VueRouter from 'vue-router'
 
-createApp(App).mount('#app')
+// Routing
+import PageHome from '@/components/PageHome'
+
+const routes = [
+  { path: '/', component: PageHome },
+]
+
+const router = VueRouter.createRouter({
+  history: VueRouter.createWebHistory(),
+  routes,
+})
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
