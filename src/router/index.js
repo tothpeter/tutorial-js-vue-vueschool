70071950d492d7c28ac5e08b75d38d49bd6c3f56
Peter Toth <tothpeter08@gmail.com>
2023-06-01 23:48:34 +0200

    Scroll to the top after editing my profile

--- src/router/index.js
+++ src/router/index.js
@@ -18,12 +18,14 @@ const routes = [
     path: '/me',
     name: 'Profile',
     component: Profile,
+    meta: { toTop: true, smoothScroll: true },
   },
   {
     path: '/me/edit',
     name: 'ProfileEdit',
     component: Profile,
     props: { edit: true },
+    meta: { toTop: true, smoothScroll: true },
   },
   {
     path: '/forum/:id',
@@ -67,4 +69,12 @@ const routes = [
 export default createRouter({
   history: createWebHistory(),
   routes,
+  scrollBehavior(to) {
+    const scroll = {}
+
+    if (to.meta.toTop) scroll.top = 0
+    if (to.meta.smoothScroll) scroll.behavior = 'smooth'
+
+    return scroll
+  }
 })
