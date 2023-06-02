f80a98aa53a475ee7b806aca050953e2b3cec282
Peter Toth <tothpeter08@gmail.com>
2023-06-02 20:06:08 +0200

    Extract the new thread form into its own component

---
+++ src/components/ThreadEditor.vue
@@ -0,0 +1,57 @@
+<template>
+  <form @submit.prevent="save">
+    <div class="form-group">
+      <label for="thread_title">Title:</label>
+      <input
+        v-model="title"
+        type="text"
+        id="thread_title"
+        class="form-input"
+        name="title"
+      />
+    </div>
+
+    <div class="form-group">
+      <label for="thread_content">Content:</label>
+      <textarea
+        v-model="text"
+        name="content"
+        id="thread_content"
+        class="form-input">
+      </textarea>
+    </div>
+
+    <div class="btn-group">
+      <button @click="$emit('cancel')" type="button" class="btn-ghost">Cancel</button>
+      <button type="submit" class="btn-blue">Save</button>
+    </div>
+  </form>
+</template>
+
+<script setup>
+import { defineProps, defineEmits } from 'vue'
+
+
+const props = defineProps({
+  title: {
+    required: false,
+    type: String
+  },
+  text: {
+    required: false,
+    type: String
+  }
+})
+
+let title = props.title || ''
+let text = props.text || ''
+
+const emit = defineEmits(['save'])
+
+function save() {
+  emit('save', {
+    title,
+    text
+  })
+}
+</script>
--- src/pages/ThreadCreate.vue
+++ src/pages/ThreadCreate.vue
@@ -2,38 +2,13 @@
   <div class="col-full push-top">
     <h1>Create a new thread in <i>{{ forum.name }}</i></h1>
 
-    <form @submit.prevent="create">
-      <div class="form-group">
-        <label for="thread_title">Title:</label>
-        <input
-          v-model="threadParams.title"
-          type="text"
-          id="thread_title"
-          class="form-input"
-          name="title"
-        />
-      </div>
-
-      <div class="form-group">
-        <label for="thread_content">Content:</label>
-        <textarea
-          v-model="postParams.text"
-          name="content"
-          id="thread_content"
-          class="form-input">
-        </textarea>
-      </div>
-
-      <div class="btn-group">
-        <button @click="cancel" type="button" class="btn-ghost">Cancel</button>
-        <button type="submit" class="btn-blue">Save</button>
-      </div>
-    </form>
+    <thread-editor @cancel="cancel" @save="create" />
   </div>
 </template>
 
 <script setup>
-import { defineProps, computed, reactive } from 'vue'
+import ThreadEditor from '@/components/ThreadEditor.vue'
+import { defineProps, computed } from 'vue'
 import { useStore } from 'vuex'
 import router from '@/router'
 
@@ -49,20 +24,20 @@ const state = store.state
 
 const forum = computed(() => { return state.forums.find(forum => forum.id === props.id) })
 
-const threadParams = reactive({
-  title: '',
-  forumId: props.id
-})
-
-const postParams = {
-  text: ''
-}
-
 function cancel() {
   router.push({ name: 'ForumShow', params: { id: props.id } })
 }
 
-async function create() {
+async function create({ title, text }) {
+  const threadParams = {
+    title,
+    forumId: props.id
+  }
+
+  const postParams = {
+    text
+  }
+
   const newThread = await store.dispatch('createThread', {
     threadParams,
     postParams
