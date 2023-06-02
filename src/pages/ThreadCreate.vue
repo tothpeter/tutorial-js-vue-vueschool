8e9556f9e2125a32f4f668af63f78391b32d0dab
Peter Toth <tothpeter08@gmail.com>
2023-06-02 18:40:38 +0200

    Create new thread

--- src/pages/ThreadCreate.vue
+++ src/pages/ThreadCreate.vue
@@ -1,12 +1,76 @@
 <template>
   <div class="col-full push-top">
-    <h1>Create a new thread in <i>{{ 'Cooking' }}</i></h1>
+    <h1>Create a new thread in <i>{{ forum.name }}</i></h1>
+
+    <form @submit.prevent="create">
+      <div class="form-group">
+        <label for="thread_title">Title:</label>
+        <input
+          v-model="newThread.title"
+          type="text"
+          id="thread_title"
+          class="form-input"
+          name="title"
+        />
+      </div>
+
+      <div class="form-group">
+        <label for="thread_content">Content:</label>
+        <textarea
+          v-model="newPost.text"
+          name="content"
+          id="thread_content"
+          class="form-input">
+        </textarea>
+      </div>
+
+      <div class="btn-group">
+        <button @click="cancel" type="button" class="btn-ghost">Cancel</button>
+        <button type="submit" class="btn-blue">Save</button>
+      </div>
+    </form>
   </div>
 </template>
 
-<script>
-export default {
+<script setup>
+import { defineProps, computed, reactive } from 'vue'
+import { useStore } from 'vuex'
+import router from '@/router'
 
+const props = defineProps({
+  id: {
+    required: true,
+    type: Number
+  }
+})
+
+const store = useStore()
+const state = store.state
+
+const forum = computed(() => { return state.forums.find(forum => forum.id === props.id) })
+
+const newThread = reactive({
+  title: 'TTTitle',
+  forumId: props.id
+})
+
+const newPost = {
+  text: 'Cont'
+}
+
+function cancel() {
+  router.push({ name: 'ForumShow', params: { id: props.id } })
+}
+
+function create() {
+  store.dispatch('createThread', {
+    post: newPost,
+    thread: newThread
+  })
+
+  router.push({ name: 'ForumShow', params: { id: props.id } })
+  // newThread.title = ''
+  // newPost.content = ''
 }
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -31,12 +31,25 @@ export default createStore({
   },
   actions: {
     createPost({ state, commit }, post) {
-      post.id = state.posts.length + 1
+      post.id = (state.posts.length + 1).toString()
       post.userId = this.getters.authUser.id
       post.publishedAt = Math.floor(Date.now() / 1000)
 
       commit('createPost', { post })
       commit('appendPostToThread', { post })
+    },
+
+    createThread({ state, commit }, { thread, post }) {
+      thread.id = (state.threads.length + 1).toString()
+      thread.userId = this.getters.authUser.id
+      thread.publishedAt = Math.floor(Date.now() / 1000)
+
+      commit('createThread', { thread })
+      commit('appendThreadToForum', { thread })
+      commit('appendThreadToUser', { thread })
+
+      post.threadId = thread.id
+      this.dispatch('createPost', post)
     },
 
     updateUser(context, user) {
@@ -47,9 +60,26 @@ export default createStore({
     createPost(state, { post }) {
       state.posts.push(post)
     },
+
+    createThread(state, { thread }) {
+      state.threads.push(thread)
+    },
+
     appendPostToThread(state, { post }) {
       const thread = state.threads.find(thread => thread.id === post.threadId)
+      thread.posts = thread.posts || []
       thread.posts.push(post.id)
+    },
+
+    appendThreadToForum(state, { thread }) {
+      const forum = state.forums.find(forum => forum.id === thread.forumId)
+      forum.threads.push(thread.id)
+    },
+
+    appendThreadToUser(state, { thread }) {
+      const user = state.users.find(user => user.id === thread.userId)
+      user.threads = user.threads || []
+      user.threads.push(thread.id)
     },
 
     updateUser(state, { user, userId }) {
