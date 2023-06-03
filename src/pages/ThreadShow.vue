be1a550308d313e3216d00535ec7b5984a38688d
Peter Toth <tothpeter08@gmail.com>
2023-06-03 19:40:20 +0200

    Use dynamic data

--- src/pages/ThreadShow.vue
+++ src/pages/ThreadShow.vue
@@ -12,7 +12,7 @@
     </h1>
 
     <p>
-      By <a href="#" class="link-unstyled">{{ thread.author.name }}</a>, 2 hours ago
+      By <a href="#" class="link-unstyled">{{ thread.author.name }}</a>, <base-date :timestamp="thread.publishedAt" />
       <span
         style="float: right; margin-top: 2px;"
         class="hide-mobile text-faded text-small"
